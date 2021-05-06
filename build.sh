5dcfc55dbe87a56b8dd5fe6c0880ffddbf473f6e
Marijn Stevens <marijnstevens@gmail.com>
2021-05-06 15:56:49 +0200

    Changes to get is compiled for armv7

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
-TARGET="aarch64-unknown-linux-gnu"
+#TARGET="aarch64-unknown-linux-gnu"
+TARGET="armv7-unknown-linux-gnueabihf"
+
 
 PROJECT_NAME="ya-runtime-vm-aarch64-host"
+
 TARGETFILE="./target/$TARGET/release/$PROJECT_NAME"
 
 echo -e "# build binary"
-#PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
+PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
 cargo build --release --target $TARGET && \
 
-echo -e $(readelf --arch-specific $TARGETFILE)
+echo -e $(readelf --arch-specific $TARGETFILE) && \
 
-mv $TARGETFILE  ../yagna-binaries/aarch64
+
+\cp -r $TARGETFILE  ../yagna-binaries/armv7/
