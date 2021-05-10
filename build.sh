8aa12fbea105e3bf952586ca32954bb23f78de83
Marijn Stevens <marijnstevens@gmail.com>
2021-05-10 18:46:56 +0200

    Push from vm

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@
 # ARCH="armv7"
 # TARGET="$ARCH-unknown-linux-gnueabihf"
 
-# TARGET_DIR="./target/$TARGET/release/"
+# TARGET_DIR="./target/$TARGET/release"
 
 # echo -e "# build binary"
 # PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
 # cargo build --release --target $TARGET && \
 
-# echo -e $(readelf --arch-specific "$TARGET_DIR/ya-runtime-vm-aarch64-host") && \
+# echo -e $(readelf --arch-specific "$TARGET_DIR/ya-runtime-vm-$ARCH-host") && \
 
-# \cp "$TARGET_DIR/ya-runtime-vm-aarch64-host"  ../yagna-binaries/$ARCH/ya-runtime-vm-$ARCH-host
-# \cp "$TARGET_DIR/libya_runtime_vm_aarch64_host.rlib" ../yagna-binaries/$ARCH/
+# \cp "$TARGET_DIR/ya-runtime-vm-$ARCH-host"  ../yagna-binaries/$ARCH/ya-runtime-vm-$ARCH-host
+# \cp "$TARGET_DIR/libya_runtime_vm_${ARCH}_host.rlib" ../yagna-binaries/$ARCH/
 # \cp "$TARGET_DIR/gvmkit" ../yagna-binaries/$ARCH/
 
