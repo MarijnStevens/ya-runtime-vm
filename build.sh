ba148d6b722532e1fa66023bbd60ba51d8929156
Marijn Stevens <marijnstevens@gmail.com>
2021-06-02 06:38:03 +0200

    Update script.

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-
 ARCH="aarch64"
 TARGET="$ARCH-unknown-linux-gnu"
-
 TARGET_DIR="./target/$TARGET/release"
 
-echo -e "# build binary"
+echo Build for $ARCH
 cargo build --release --target $TARGET && \
-
-echo -e $(readelf --arch-specific "$TARGET_DIR/ya-runtime-vm-$ARCH-host")
 
 \cp "$TARGET_DIR/ya-runtime-vm-$ARCH-host"  ../yagna-binaries/plugins/ya-runtime-vm-aarch64-host/ya-runtime-vm-$ARCH-host
 \cp "$TARGET_DIR/libya_runtime_vm_${ARCH}_host.rlib" ../yagna-binaries/
