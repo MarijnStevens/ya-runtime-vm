2b01afe30f548c6d876e474f70297b067774fda1
Marijn Stevens <marijnstevens@gmail.com>
2021-06-06 23:25:09 +0200

    Cleanup. We dont need rlib.

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ echo Build for $ARCH
 cargo build --release --target $TARGET && \
 
 \cp "$TARGET_DIR/ya-runtime-vm-$ARCH-host"  ../yagna-binaries/plugins/ya-runtime-vm-aarch64-host/ya-runtime-vm-$ARCH-host
-\cp "$TARGET_DIR/libya_runtime_vm_${ARCH}_host.rlib" ../yagna-binaries/
-\cp "$TARGET_DIR/gvmkit" ../yagna-binaries/
+
+# We dont need gvmkit atm.
+#\cp "$TARGET_DIR/gvmkit" ../yagna-binaries/
 
