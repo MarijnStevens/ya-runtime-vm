565d537601e5d9cd8055770b0a7cfbbc6e5dbc9e
Marijn Stevens <marijnstevens@gmail.com>
2021-06-07 15:21:21 +0200

    Debugging; why doesnt github action throws an error on exit 1 ?

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ TARGET_DIR="./target/$TARGET/release"
 echo Build for $ARCH
 cargo build --release --target $TARGET && \
 
+ls && \
+
 \cp "$TARGET_DIR/ya-runtime-vm-$ARCH-host"  ../yagna-binaries/plugins/ya-runtime-vm-aarch64-host/ya-runtime-vm-$ARCH-host || exit 1
 
 # We dont need gvmkit atm.
