319e8bae01b767e87af7b22ff55843734a2a14e3
Marijn Stevens <marijnstevens@gmail.com>
2021-06-07 16:42:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ TARGET_DIR="./target/$TARGET/release"
 echo Build for $ARCH
 cargo build --release --target $TARGET && \
 
-ls && \
+ls ./target/ && \
 
 \cp "$TARGET_DIR/ya-runtime-vm-$ARCH-host"  ../yagna-binaries/plugins/ya-runtime-vm-aarch64-host/ya-runtime-vm-$ARCH-host || exit 1
 
